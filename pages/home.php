c84344661fae019bb80501f0d2b8ce747cb9f748
Yusuf Babatunde Yusuf <105415149+Boy2700@users.noreply.github.com>
2024-06-06 18:53:23 +0100

    Attendance management System

---
+++ pages/home.php
@@ -0,0 +1,15 @@
+<center>
+<img src="https://fudutsinma.edu.ng/wp-content/uploads/2022/10/logo-1.png" alt="fudma" height="100px" width="100">
+</center>
+
+<center>
+<div class="page-title">Attendance Management System</div>
+
+</center>
+<hr>
+<footer>
+    <center>
+    <b>  <p>Allright reserved &copy; 2024 Attendance Management System || David</p> <b>
+    </center>
+   
+</footer>
